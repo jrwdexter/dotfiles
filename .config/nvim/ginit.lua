014992733375b213b34bd232ea679d6454b56a3f
Jonathan Dexter <mandest@gmail.com>
2021-04-13 13:26:01 -0500

    Added mappings for editing gvimrc

--- .config/nvim/ginit.lua
+++ .config/nvim/ginit.lua
@@ -3,10 +3,11 @@ vim.api.nvim_command('GuiFont! JetBrains\\ Mono:h11')
 vim.api.nvim_command('GuiPopupmenu 0')
 
 -- Mappings
--- vim.g.my_gvimrc=substitute($MYVIMRC, 'init.vim$', 'ginit.vim', 'g')
+local vimrc = os.getenv('MYVIMRC')
+vim.g.my_gvimrc=vimrc:gsub('init.lua', 'ginit.lua')
 
--- nnoremap <leader>eg :execute 'edit' g:my_gvimrc<CR>
--- nnoremap <leader>sg :execute 'source' g:my_gvimrc<CR>
+vim.api.nvim_set_keymap('n', '<leader>eg', ":execute 'edit' g:my_gvimrc<cr>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>sg', ":execute luafile g:my_gvimrc<CR>:filetype detect<CR>:echo 'vimrc reloaded'<CR>", { noremap = true, silent = true })
 
 -- -- Colors
 -- colorscheme dracula
