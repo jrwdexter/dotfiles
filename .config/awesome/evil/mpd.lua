ae240dc34dc49bd57a98fc6f252d4699b4bc282f
Jonathan Dexter <jonthan.dexter@monkeyjumplabs.com>
2022-03-23 10:59:25 -0500

    Various mpd awesome updates

--- .config/awesome/evil/mpd.lua
+++ .config/awesome/evil/mpd.lua
@@ -10,33 +10,52 @@
 --      random (boolean)
 local awful = require("awful")
 
+local function start_script(script, attempt, emit_function)
+    awful.spawn.with_line_callback(script, {
+        stdout = function()
+            emit_function()
+        end,
+        stderr = function()
+            emit_function()
+        end,
+        exit = function(_, exit_code)
+            if(attempt > 10) then
+                return
+            end
+            awful.spawn.easy_async_with_shell("sleep 15", function()
+                if (exit_code == 1) then
+                    start_script(script, attempt+1, emit_function)
+                end
+            end)
+        end
+    });
+end
+
 local function emit_info()
     awful.spawn.easy_async_with_shell("sh -c 'mpc -f ARTIST@%artist%@TITLE@%title%@FILE@%file%@'",
-        function(stdout, _, _, exitcode)
-            if exitcode == 0 then
-                local artist = stdout:match('^ARTIST@(.*)@TITLE')
-                local title = stdout:match('@TITLE@(.*)@FILE')
-                local status = stdout:match('\n%[(.*)%]')
+        function(stdout)
+            local artist = stdout:match('^ARTIST@(.*)@TITLE')
+            local title = stdout:match('@TITLE@(.*)@FILE')
+            local status = stdout:match('\n%[(.*)%]')
 
-                if not artist or artist == "" then
-                  artist = "N/A"
-                end
-                if not title or title == "" then
-                  title = stdout:match('@FILE@(.*)@')
-                  if not title or title == "" then
-                      title = "N/A"
-                  end
-                end
-
-                local paused
-                if status == "playing" then
-                    paused = false
-                else
-                    paused = true
-                end
-
-                awesome.emit_signal("evil::mpd", artist, title, paused)
+            if not artist or artist == "" then
+              artist = "N/A"
             end
+            if not title or title == "" then
+              title = stdout:match('@FILE@(.*)@')
+              if not title or title == "" then
+                  title = "N/A"
+              end
+            end
+
+            local paused
+            if status == "playing" then
+                paused = false
+            else
+                paused = true
+            end
+
+            awesome.emit_signal("evil::mpd", artist, title, paused)
         end
     )
 end
@@ -52,12 +71,7 @@ local mpd_script = [[
 
 -- Kill old mpc idleloop player process
 awful.spawn.easy_async_with_shell("ps x | grep \"mpc idleloop player\" | grep -v grep | awk '{print $1}' | xargs kill", function ()
-    -- Emit song info with each line printed
-    awful.spawn.with_line_callback(mpd_script, {
-        stdout = function()
-            emit_info()
-        end
-    })
+    start_script(mpd_script, 0, emit_info)
 end)
 
 ----------------------------------------------------------
@@ -65,10 +79,8 @@ end)
 -- MPD Volume
 local function emit_volume_info()
     awful.spawn.easy_async_with_shell("mpc volume | awk '{print substr($2, 1, length($2)-1)}'",
-        function(stdout, _, _, exitcode)
-            if exitcode == 0 then
-                awesome.emit_signal("evil::mpd_volume", tonumber(stdout))
-            end
+        function(stdout)
+            awesome.emit_signal("evil::mpd_volume", tonumber(stdout))
         end
     )
 end
@@ -88,12 +100,8 @@ local mpd_volume_script = [[
 
 -- Kill old mpc idleloop mixer process
 awful.spawn.easy_async_with_shell("ps x | grep \"mpc idleloop mixer\" | grep -v grep | awk '{print $1}' | xargs kill", function ()
-    -- Emit song info with each line printed
-    awful.spawn.with_line_callback(mpd_volume_script, {
-        stdout = function()
-            emit_volume_info()
-        end
-    })
+    -- Emit volume info with each line printed
+    start_script(mpd_volume_script, 0, emit_volume_info)
 end)
 
 local mpd_options_script = [[
@@ -103,13 +111,11 @@ local mpd_options_script = [[
 
 local function emit_options_info()
     awful.spawn.easy_async_with_shell("mpc | tail -1",
-        function(stdout, _, _, exitcode)
-            if exitcode == 0 then
-                local loop = stdout:match('repeat: (.*)')
-                local random = stdout:match('random: (.*)')
-                if not (loop == nil) and not (random == nil) then
-                    awesome.emit_signal("evil::mpd_options", loop:sub(1, 2) == "on", random:sub(1, 2) == "on")
-                end
+        function(stdout)
+            local loop = stdout:match('repeat: (.*)')
+            local random = stdout:match('random: (.*)')
+            if (loop ~= nil and random ~= nil)  then
+              awesome.emit_signal("evil::mpd_options", loop:sub(1, 2) == "on", random:sub(1, 2) == "on")
             end
         end
     )
@@ -121,9 +127,5 @@ emit_options_info()
 -- Kill old mpc idleloop options process
 awful.spawn.easy_async_with_shell("ps x | grep \"mpc idleloop options\" | grep -v grep | awk '{print $1}' | xargs kill", function ()
     -- Emit song info with each line printed
-    awful.spawn.with_line_callback(mpd_options_script, {
-        stdout = function()
-            emit_options_info()
-        end
-    })
+    start_script(mpd_options_script, 0, emit_options_info)
 end)
