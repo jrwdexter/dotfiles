4613c1d13ea88dfd798ef4b477d4f19fa91afad6
jrwdexter <jrwdexter@gmail.com>
2023-05-05 23:24:02 -0500

    Added azure pipelines language server

--- .config/nvim/lua/ide/lsp.lua
+++ .config/nvim/lua/ide/lsp.lua
@@ -2,60 +2,64 @@ local lsp = {}
 
 local function run_command(cmd)
   local proc = io.popen(cmd)
-  local result = proc:read('*all')
+  local result = proc:read("*all")
   local success, _, exit_code = proc:close()
-  return result, (success and result ~= ''), exit_code
+  return result, (success and result ~= ""), exit_code
 end
 
 local function find_file(file)
-  if (vim.fn.executable('readlink') ~= 0) and (vim.fn.executable('which') ~= 0) then
-    return run_command('readlink -f `which '..file..'` | head -n 1')
-  elseif vim.fn.executable('which') then
-    return run_command('which '..file)
+  if (vim.fn.executable("readlink") ~= 0) and (vim.fn.executable("which") ~= 0) then
+    return run_command("readlink -f `which " .. file .. "` | head -n 1")
+  elseif vim.fn.executable("which") then
+    return run_command("which " .. file)
   end
-  return '', false, nil
+  return "", false, nil
 end
 
 ----Fix for not spawning things correctly on windows
 --vim.loop.spawn = (function ()
-  --local spawn = vim.loop.spawn
-  --return function(path, options, on_exit)
-    --local full_path = vim.fn.exepath(path)
-    --return spawn(full_path, options, on_exit)
-  --end
+--local spawn = vim.loop.spawn
+--return function(path, options, on_exit)
+--local full_path = vim.fn.exepath(path)
+--return spawn(full_path, options, on_exit)
+--end
 --end)()
 
 lsp.startup = function(use)
-  use { 'neovim/nvim-lspconfig' }
+  use({ "neovim/nvim-lspconfig" })
 end
 
 lsp.init = function(capabilities)
-  local lspconfig = require('lspconfig')
+  local lspconfig = require("lspconfig")
 
   local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    local function buf_set_keymap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+      vim.api.nvim_buf_set_option(bufnr, ...)
+    end
 
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-S-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('i', '<C-S-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    local opts = { noremap = true, silent = true }
+    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<C-S-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("i", "<C-S-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
     if client.server_capabilities.document_formatting then
@@ -67,16 +71,16 @@ lsp.init = function(capabilities)
 
     ---- Set autocommands conditional on server_capabilities
     --if client.resolved_capabilities.document_highlight then
-      --vim.api.nvim_exec([[
-        --hi LspReferenceRead cterm=bold ctermbg=red guibg=DarkGray
-        --hi LspReferenceText cterm=bold ctermbg=red guibg=DarkGray
-        --hi LspReferenceWrite cterm=bold ctermbg=red guibg=DarkGray
-        --augroup lsp_document_highlight
-          --autocmd! * <buffer>
-          --autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-          --autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        --augroup END
-      --]], false)
+    --vim.api.nvim_exec([[
+    --hi LspReferenceRead cterm=bold ctermbg=red guibg=DarkGray
+    --hi LspReferenceText cterm=bold ctermbg=red guibg=DarkGray
+    --hi LspReferenceWrite cterm=bold ctermbg=red guibg=DarkGray
+    --augroup lsp_document_highlight
+    --autocmd! * <buffer>
+    --autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+    --autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    --augroup END
+    --]], false)
     --end
   end
 
@@ -84,51 +88,66 @@ lsp.init = function(capabilities)
   --capabilities.textDocument.completion.completionItem.snippetSupport = true
 
   -- LSP: C#
-  local omnisharp_bin, success, _ = find_file('omnisharp')
+  local omnisharp_bin, success, _ = find_file("omnisharp")
   if success then
     omnisharp_bin = omnisharp_bin:gsub("^%s*(.-)%s*$", "%1") -- trim
     local util = lspconfig.util
     local pid = vim.fn.getpid()
-    lspconfig.omnisharp.setup{
+    lspconfig.omnisharp.setup({
       capabilities = capabilities,
-      filetypes = { 'cs', 'csx', 'fs', 'fsx', 'vb' },
-      cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
+      filetypes = { "cs", "csx", "fs", "fsx", "vb" },
+      cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
       on_attach = on_attach,
-      root_dir = function(fname) 
-        local dir = util.root_pattern '*.sln'(fname) or util.root_pattern '*.csproj'(fname) or util.root_pattern 'omnisharp.json'(fname)
+      root_dir = function(fname)
+        local dir = util.root_pattern("*.sln")(fname)
+          or util.root_pattern("*.csproj")(fname)
+          or util.root_pattern("omnisharp.json")(fname)
         return dir
-      end
-    }
+      end,
+    })
   end
 
   -- LSP: Typescript
-  require'lspconfig'.tsserver.setup{
+  require("lspconfig").tsserver.setup({
     capabilities = capabilities,
-    on_attach = on_attach
-  }
+    on_attach = on_attach,
+  })
 
   -- LSP: CSS
-  require'lspconfig'.cssls.setup {
+  require("lspconfig").cssls.setup({
     capabilities = capabilities,
-    on_attach = on_attach
-  }
+    on_attach = on_attach,
+  })
 
   -- LSP: Docker
-  require'lspconfig'.dockerls.setup{
+  require("lspconfig").dockerls.setup({
     capabilities = capabilities,
-    on_attach = on_attach
-  }
+    on_attach = on_attach,
+  })
 
   -- LSP: PYTHON
   -- Python has an automatic cmd
-  lspconfig.pylsp.setup{
+  lspconfig.pylsp.setup({
     capabilities = capabilities,
-    on_attach = on_attach
-  }
+    on_attach = on_attach,
+  })
 
-  require'lspconfig'.yamlls.setup{
+  require("lspconfig").azure_pipelines_ls.setup({
+    settings = {
+      yaml = {
+        schemas = {
+          ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+            "/azure-pipeline*.y*l",
+            "/deploy.y*l",
+          },
+        },
+      },
+    },
+  })
+
+  require("lspconfig").yamlls.setup({
     capabilities = capabilities,
-    filetypes = { 'yaml', 'yaml.docker-compose', 'helm' },
+    filetypes = { "yaml", "yaml.docker-compose", "helm" },
     on_attach = function(client, bufnr)
       on_attach(client, bufnr)
       if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
@@ -137,49 +156,54 @@ lsp.init = function(capabilities)
     end,
     settings = {
       cmd = { "yaml-language-server.cmd", "--stdio" },
-      schemas = {
-            ["https://json.schemastore.org/chart.json"] = "/deployment/helm/*",
-            ["https://json.schemastore.org/chart.json"] = "**/k8s/**.yaml",
-            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+      yaml = {
+        schemas = {
+          ["https://json.schemastore.org/chart.json"] = {"/deployment/helm/*", "**/k8s/**.yaml"},
+          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+          ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+            "/azure-pipeline*.y*l",
+            "/deploy.y*l",
+          },
+        },
       },
-    }
-  }
+    },
+  })
 
   -- LSP: Haskell
   -- TODO
-  local hls_bin, hls_success, _ = find_file('haskell-language-server-wrapper')
+  local hls_bin, hls_success, _ = find_file("haskell-language-server-wrapper")
   if hls_success then
-    require'lspconfig'.hls.setup{
-      on_attach = on_attach
-    }
+    require("lspconfig").hls.setup({
+      on_attach = on_attach,
+    })
   end
 
   -- LSP: LUA
-  local luals_bin, lua_success, _ = find_file('lua-language-server')
+  local luals_bin, lua_success, _ = find_file("lua-language-server")
   if lua_success then
-    luals_bin = luals_bin:gsub('%s+$', '')
-    local luals_bin_dir, _, _ = run_command('dirname '..luals_bin)
-    luals_bin_dir = luals_bin_dir:gsub('%s+$', '')
+    luals_bin = luals_bin:gsub("%s+$", "")
+    local luals_bin_dir, _, _ = run_command("dirname " .. luals_bin)
+    luals_bin_dir = luals_bin_dir:gsub("%s+$", "")
 
-    lspconfig.lua_ls.setup {
+    lspconfig.lua_ls.setup({
       capabilities = capabilities,
       settings = {
         Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
+            version = "LuaJIT",
             -- Setup your lua path
-            path = vim.split(package.path, ';'),
+            path = vim.split(package.path, ";"),
           },
           diagnostics = {
             -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
+            globals = { "vim" },
           },
           workspace = {
             -- Make the server aware of Neovim runtime files
             library = {
-              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
             },
           },
           -- Do not send telemetry data containing a randomized but unique identifier
@@ -188,8 +212,8 @@ lsp.init = function(capabilities)
           },
         },
       },
-      on_attach = on_attach
-    }
+      on_attach = on_attach,
+    })
   end
 end
 
