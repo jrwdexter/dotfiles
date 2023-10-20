72e837dc3a36e543dfd098be99fe75b7fb9abb22
jrwdexter <jrwdexter@gmail.com>
2023-10-20 17:19:08 -0500

    Updated azure pipelines lsp

--- .config/nvim/lua/ide/lsp.lua
+++ .config/nvim/lua/ide/lsp.lua
@@ -138,7 +138,9 @@ lsp.init = function(capabilities)
         schemas = {
           ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
             "/azure-pipeline*.y*l",
-            "/deploy.y*l",
+            "/*.azure*",
+            "Azure-Pipelines/**/*.y*l",
+            "Pipelines/*.y*l",
           },
         },
       },
