c919ca7dce620825e1b79cba1e1c4115bcab9809
jrwdexter <jrwdexter@gmail.com>
2023-10-20 21:16:44 -0500

    Slight rename of globals (note: reloading doesn't work right now)

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -1,34 +1,26 @@
-local globals = { }
+Globals = {}
 
-globals.init = function()
-  local funcs = {}
+Globals.reload_vimrc = function()
+  -- TODO: replace these with some calculated way to unload these packages
+  --package.loaded['git'] = nil
+  --package.loaded['globals'] = nil
+  --package.loaded['hotkeys'] = nil
+  --package.loaded['ide'] = nil
+  --package.loaded['language'] = nil
+  --package.loaded['settings'] = nil
+  --package.loaded['style'] = nil
+  --package.loaded['utils'] = nil
+  --package.loaded['ide.autocomplete'] = nil
+  --package.loaded['ide.fzf'] = nil
+  --package.loaded['ide.lsp'] = nil
+  --package.loaded['ide.run'] = nil
+  --package.loaded['ide.status'] = nil
+  --package.loaded['ide.tree'] = nil
+  local vimrc = vim.env.MYVIMRC
+  dofile(vimrc)
 
-  funcs.reload_vimrc = function()
-    -- TODO: replace these with some calculated way to unload these packages
-    package.loaded['git'] = nil
-    package.loaded['globals'] = nil
-    package.loaded['hotkeys'] = nil
-    package.loaded['ide'] = nil
-    package.loaded['language'] = nil
-    package.loaded['settings'] = nil
-    package.loaded['style'] = nil
-    package.loaded['utils'] = nil
-    package.loaded['ide.autocomplete'] = nil
-    package.loaded['ide.fzf'] = nil
-    package.loaded['ide.lsp'] = nil
-    package.loaded['ide.run'] = nil
-    package.loaded['ide.status'] = nil
-    package.loaded['ide.tree'] = nil
-    local vimrc = vim.env.MYVIMRC
-    dofile(vimrc)
-
-    if vim.g.GuiLoaded then
-      local gvimrc = vimrc:gsub('init.lua', 'ginit.lua')
-      dofile(gvimrc)
-    end
+  if vim.g.GuiLoaded then
+    local gvimrc = vimrc:gsub("init.lua", "ginit.lua")
+    dofile(gvimrc)
   end
-
-  return funcs
 end
-
-return globals
